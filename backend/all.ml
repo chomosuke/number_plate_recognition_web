4d16472ca1aa42432620ee1516cb419e8555bad3
chomosuke <a13323600@gmail.com>
2023-11-20 15:08:38 +1100

    fetched all docs unprocessed

--- backend/all.ml
+++ backend/all.ml
@@ -3,10 +3,18 @@ open! Async
 open! Cohttp
 open! Cohttp_async
 module Json = Yojson.Basic
-
-let get_all_docs () = Db.query "/number_plates/_all_docs"
+open! Json.Util
 
 let get _body _req =
-  let%bind all_docs = get_all_docs () in
-  Server.respond_string (Json.to_string all_docs)
+  let%bind all_docs = Db.query "/number_plates/_all_docs" in
+  let doc_ids =
+    member "rows" all_docs
+    |> to_list
+    |> List.map ~f:(fun doc -> doc |> member "id" |> to_string)
+  in
+  let%bind docs =
+    List.map doc_ids ~f:(fun id -> Db.query ("/number_plates/" ^ id)) |> Deferred.all
+  in
+  let docs = `List docs in
+  Server.respond_string (Json.to_string docs)
 ;;
