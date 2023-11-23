73bdd5fb6dcc5ad5f0235440f4fd8cb55af618f5
chomosuke <a13323600@gmail.com>
2023-11-23 19:30:52 +1100

    styled table slightly

--- frontend/table.ml
+++ frontend/table.ml
@@ -9,19 +9,8 @@ let component =
         type t = Get_plates.plate list [@@deriving sexp, equal]
       end)
   in
-  (* let%sub chosen_image, set_chosen_image = *)
-  (*   Bonsai.state_opt *)
-  (*     (module struct *)
-  (*       type t = string [@@deriving sexp, equal] *)
-  (*     end) *)
-  (*     ~default_model:None *)
-  (* in *)
   let%sub on_activate =
-    let%arr set_plates =
-      set_plates
-      (* and chosen_image = chosen_image *)
-      (* and set_chosen_image = set_chosen_image *)
-    in
+    let%arr set_plates = set_plates in
     let%bind.Effect plates = Get_plates.get () in
     set_plates (Some plates)
   in
@@ -30,19 +19,29 @@ let component =
   let open Vdom in
   match plates with
   | Some plates ->
-    Node.table
+    Node.table ~attrs:[ Attr.style Css_gen.(border_collapse `Collapse) ]
     @@ List.map plates ~f:(fun { number; date_time; image } ->
       Node.tr
       @@ List.map
-           ~f:(fun n -> Node.td [ n ])
+           ~f:(fun n ->
+             Node.td
+               ~attrs:
+                 [ Attr.style
+                     Css_gen.(
+                       padding
+                         ~top:(`Px 5)
+                         ~bottom:(`Px 5)
+                         ~left:(`Px 10)
+                         ~right:(`Px 10)
+                         ()
+                       @> border ~style:`Solid ~width:(`Px 1) ())
+                 ]
+               [ n ])
            [ Node.text number
            ; Node.text date_time
            ; Node.img
                ~attrs:
-                 [ Attr.src @@ "/api/" ^ image
-                 ; Attr.style
-                     Css_gen.(height (`Px 80))
-                 ]
+                 [ Attr.src @@ "/api/" ^ image; Attr.style Css_gen.(height (`Px 80)) ]
                ()
            ])
   | None -> Node.text "Loading..."
