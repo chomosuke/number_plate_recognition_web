059eda88c70615d6a5e98aec0eb46d77a4f8c452
chomosuke <a13323600@gmail.com>
2023-11-27 16:55:02 +1100

    implemented login
    need to obsture password

--- frontend/login.ml
+++ frontend/login.ml
@@ -3,12 +3,46 @@ open! Bonsai_web
 open! Bonsai.Let_syntax
 
 let component =
-  Computation.return (fun on_login ->
+  let%sub username, set_username =
+    Bonsai.state
+      (module struct
+        type t = string [@@deriving sexp, equal]
+      end)
+      ~default_model:""
+  in
+  let%sub password, set_password =
+    Bonsai.state
+      (module struct
+        type t = string [@@deriving sexp, equal]
+      end)
+      ~default_model:""
+  in
+  let%arr username = username
+  and set_username = set_username
+  and password = password
+  and set_password = set_password in
+  fun on_login ->
     let open Vdom in
-    Node.button
-      ~attrs:
-        [ Attr.on_click (fun _ ->
-            on_login ({ username = "admin"; password = "password" } : Http.login))
-        ]
-      [ Node.text "login" ])
+    Node.div
+      [ Node.text "username: "
+      ; Node.input
+          ~attrs:
+            [ Attr.on_change (fun _ str -> set_username str)
+            ; Attr.style Css_gen.(margin ~bottom:(`Px 30) ())
+            ]
+          ()
+      ; Node.br ()
+      ; Node.text "password: "
+      ; Node.input
+          ~attrs:
+            [ Attr.on_change (fun _ str -> set_password str)
+            ; Attr.style Css_gen.(margin ~bottom:(`Px 30) ())
+            ]
+          ()
+      ; Node.br ()
+      ; Node.button
+          ~attrs:
+            [ Attr.on_click (fun _ -> on_login ({ username; password } : Http.login)) ]
+          [ Node.text "login" ]
+      ]
 ;;
